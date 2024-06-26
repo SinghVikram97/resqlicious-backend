cb4df51890e71659e0143b82b0d717468ec05d07
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2024-06-26 10:55:42 -0400

    project setup

---
+++ src/main/java/com/vikram/resqliciousbackend/ResqliciousBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vikram.resqliciousbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResqliciousBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResqliciousBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vikram/resqliciousbackend/ResqliciousBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikram.resqliciousbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResqliciousBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
